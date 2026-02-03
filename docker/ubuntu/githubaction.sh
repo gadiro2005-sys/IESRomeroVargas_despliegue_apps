d476bae29a8f540e1758d1dfd3bd81d0cc54fc9f
Carlos <carlos@email.com>
2026-02-03 17:53:46 +0100

    Cambio el token del runner

--- docker/ubuntu/githubaction.sh
+++ docker/ubuntu/githubaction.sh
@@ -8,7 +8,7 @@ sudo service nginx start
 # único al registrar el runner de github
 # formato: timestamp + random number
 UNIQUE_ID="$(date +%s)-$RANDOM"
-TOKEN="B2733TNEULZDLXO6DIIT44TJQEG7Y"
+TOKEN="B2733TPDIYZFCB6HNBDSBUTJQI3A6"
 GITHUB_REPOSITORY="https://github.com/gadiro2005-sys/IESRomeroVargas_despliegue_apps"
 RUNNER_PATH="/home/runner"
 CONTAINER_NAME="server-$UNIQUE_ID"
